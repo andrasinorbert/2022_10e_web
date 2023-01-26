3229bfb475627fa15acc1e30d4658de22889f3fe
andrasinorbert92 <113181319+andrasinorbert@users.noreply.github.com>
2023-01-26 14:31:44 +0100

    20230126_korrep

---
+++ 20230126/fvek.js
@@ -0,0 +1,79 @@
+function $(id){
+    return document.getElementById(id);
+}
+
+
+
+function caption_create(cap){
+    let start_tag= "<caption>";
+    let end_tag= "</caption>";
+    let tartalom=cap;
+    return start_tag+tartalom+end_tag;
+}
+
+function th_create(col_name){
+    let start_tag= "<th>";
+    let end_tag= "</th>";
+    let tartalom=col_name;
+    return start_tag+tartalom+end_tag;
+
+}
+
+function tr_create(col_num, col_names){
+    let start_tag= "<tr>";
+    let end_tag= "</tr>";
+    let tartalom="";
+    for(let i=0; i< col_num; ++i){
+        tartalom+=th_create(col_names[i]);
+    }
+    return start_tag+tartalom+end_tag;
+}
+
+function thead_create(col_num, col_names){
+    let start_tag= "<thead>";
+    let end_tag= "</thead>";
+    let tartalom=tr_create(col_num, col_names);
+    return start_tag+tartalom+end_tag;
+}
+
+function tablazat_create(col_num, row_num, cap, col, data){
+    let start_tag= "<table>";
+    let end_tag= "</table>";
+
+
+
+    let tartalom=caption_create(cap);
+    tartalom+=thead_create(col_num, col);
+
+
+    return start_tag+tartalom+end_tag;
+}
+
+/*
+<table>
+    <caption>Táblázat</caption>
+    <thead>
+        <tr>
+            <th>oszlop1</th>
+            <th>oszlop2</th>
+            <th>oszlop3</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>sor1oszlop1</td>
+            <td>sor1oszlop2</td>
+            <td>sor1oszlop3</td>
+        </tr>
+        <tr>
+            <td>sor2oszlop1</td>
+            <td>sor2oszlop2</td>
+            <td>sor2oszlop3</td>
+        </tr>
+    </tbody>
+</table>
+*/
+
+function tablazat_visible(id, col_num, row_num, cap, col, data){
+    $(id).innerHTML=tablazat_create(col_num, row_num, cap, col, data);
+}
---
+++ 20230126/main.js
@@ -0,0 +1,16 @@
+
+
+var caption="A táblázat cime";
+var oszlopnevek = [ "oszlop1", "oszlop2", "oszlop3"];
+var adatok=[
+    ["sor1oszlop1", "sor1oszlop2", "sor1oszlop3"],
+    ["sor2oszlop1", "sor2oszlop2", "sor2oszlop3"]
+];
+
+var oszlopszam=3;
+var sorszam=2;
+
+var div_id="tablazat";
+
+tablazat_visible(div_id, oszlopszam, sorszam, caption, oszlopnevek, adatok);
+
